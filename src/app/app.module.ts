2c415046b632300f4ecd2b78839e362df24755bb
Anna1497 <35584524+Anna1497@users.noreply.github.com>
2018-09-24 09:35:20 -0500

    segundo commit
    ya con toda la informacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,51 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AboutPage } from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DishProvider } from '../providers/dish/dish';
+import { LeaderProvider } from '../providers/leader/leader';
+import { PromotionProvider } from '../providers/promotion/promotion';
+import { HttpClientModule } from '@angular/common/http';
+
+import {dbURL} from '../share/dburl';
 
 @NgModule({
+  
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AboutPage,
+    MenuPage,
+    ContactPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AboutPage,
+    MenuPage,
+    ContactPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DishProvider,
+    LeaderProvider,
+    PromotionProvider,
+    {provide: 'DbURL', useValue: dbURL}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { Component, OnInit,Inject } from '@angular/core';
+import { NavController} from 'ionic-angular';
+import { DishProvider} from '../../providers/dish/dish';
+
+import {Dish} from '../../share/dish';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  dish: Dish;
 
+  constructor(
+    public navCtrl: NavController,
+    private dishService: DishProvider
+   @Inject('DbURL') private dbURL
+  ){}
+
+  ngOnInit(){
+
+  }  
+  getFeaturedDish(){
+    this.dishService
+    .getFeaturedDish()
+    .subscribe(
+      response => {
+        this.dish = response[0];
+        console.log(this.dish);
+      }
+    );
   }
-
 }
+  /* ES UNA INSTANCIA DEL PROVIDER QUE CREAMOS EN EL CONSTRUCTOR ( THIS) POR QUE ESTAMOS EN LA MISMA 
+  CLASE // GETFEA
+TUREDDISH = metodo(funcion) y se manda a llamar  y se suscribe a una promesa 
+  ( a que le regrese un valor)  cacha 0 que es el valor del arreglo y la respuesta o el error lo 
+  marca en la consola */
+
+
+
